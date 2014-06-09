8aa0300481f43c6831814d39c67ead1ba52a92a9
William Makley <wmakley@tortus.com>
2014-06-09 12:37:37 -0400

    fix typo in table name

--- db/migrate/2_add_featured_to_faqs_faqs.rb
+++ db/migrate/2_add_featured_to_faqs_faqs.rb
@@ -1,7 +1,7 @@
 class AddFeaturedToFaqsFaqs < ActiveRecord::Migration
 
   def change
-    add_column :refinery_faqs_faqs, :featured, :boolean, :null => false, :default => false
+    add_column :refinery_faqs, :featured, :boolean, :null => false, :default => false
   end
 
 end
