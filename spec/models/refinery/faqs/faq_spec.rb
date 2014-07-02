2b142ce3cbe1f347f506e93c769107afa196e14a
William Makley <wmakley@tortus.com>
2014-07-02 17:48:10 -0400

    fix broken faq spec

--- spec/models/refinery/faqs/faq_spec.rb
+++ spec/models/refinery/faqs/faq_spec.rb
@@ -3,17 +3,15 @@ require 'spec_helper'
 module Refinery
   module Faqs
     describe Faq do
-      describe "validations" do
-        subject {
-          FactoryGirl.build(:faq)
-        }
+      subject { FactoryGirl.build(:faq) }
 
+      describe "validations" do
         it { should be_valid }
         its(:errors) { should be_empty }
       end
 
       it "saves" do
-        subject.save.should be_truthy
+        subject.save!.should be_truthy
       end
     end
   end
